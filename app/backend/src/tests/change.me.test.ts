114c65bde41e18261bbec3e80c11367eaca44a13
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-07-28 15:41:12 -0300

    .

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -4,7 +4,7 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import Example from '../database/models/ExampleModel';
+import User from '../database/models/User';
 
 import { Response } from 'superagent';
 
@@ -13,18 +13,18 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Seu teste', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
+  // /**
+  //  * Exemplo do uso de stubs com tipos
+  //  */
 
   // let chaiHttpResponse: Response;
 
   // before(async () => {
   //   sinon
-  //     .stub(Example, "findOne")
+  //     .stub(User, "findOne")
   //     .resolves({
   //       ...<Seu mock>
-  //     } as Example);
+  //     } as User);
   // });
 
   // after(()=>{
@@ -39,7 +39,7 @@ describe('Seu teste', () => {
   //   expect(...)
   // });
 
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
-  });
+  // it('Seu sub-teste', () => {
+  //   expect(false).to.be.eq(true);
+  // });
 });
